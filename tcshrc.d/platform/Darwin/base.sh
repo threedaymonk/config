b053061c0b4ae926f1a8bd59b9b8161c64b87cf0
Paul Battley <pbattley@gmail.com>
2009-04-29 12:54:13 +0000

    Updated paths for Leopard.

--- tcshrc.d/platform/Darwin/base.sh
+++ tcshrc.d/platform/Darwin/base.sh
@@ -1,1 +1,1 @@
-setenv PATH ${HOME}/bin:/opt/local/bin:/opt/local/sbin:/usr/local/mysql/bin:/usr/local/bin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/X11R6/bin:/opt/local/Library/Frameworks/Python.framework/Versions/2.4/bin
+setenv PATH ${HOME}/bin:/opt/local/bin:/opt/local/sbin:/usr/local/mysql/bin:/usr/local/bin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/X11R6/bin:/opt/local/Library/Frameworks/Python.framework/Versions/2.4/bin:/usr/local/git/bin:/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin
