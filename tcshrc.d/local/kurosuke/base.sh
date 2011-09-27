905a1bd35a2c104feb89d90334eceedd4202034f
Paul Battley <pbattley@gmail.com>
2011-09-27 10:23:51 +0100

    Add Android platform tools to path.

--- tcshrc.d/local/kurosuke/base.sh
+++ tcshrc.d/local/kurosuke/base.sh
@@ -1,3 +1,4 @@
 set -l path = (${HOME}/.gem/ruby/1.8/bin $path)
 set -l path = (${HOME}/local/bin $path)
 set -l path = (/opt/android-sdk-linux_x86/tools $path)
+set -l path = (/opt/android-sdk-linux_x86/platform-tools $path)
