0b072114407bba7fb05728508ca6eaba307d487a
Paul Battley <pbattley@gmail.com>
2012-08-02 15:48:49 +0100

    Handle symlinked directories

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 origin=$(pwd)
 cd $HOME
 for f in ackrc ctags tmux.conf vimrc vim zshenv zshrc; do
-  ln -sf "$origin/$f" ".$f"
+  ln -sfn "$origin/$f" ".$f"
 done
 
 cd $origin/config
 for f in *; do
-  ln -sf $origin/config/$f $HOME/.config/$f
+  ln -sfn $origin/config/$f $HOME/.config/$f
 done
