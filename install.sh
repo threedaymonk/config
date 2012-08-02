4906ec675506cdc03b461912eb1057405b71ecd5
Paul Battley <pbattley@gmail.com>
2012-08-02 15:47:16 +0100

    Configure Roxterm

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ cd $HOME
 for f in ackrc ctags tmux.conf vimrc vim zshenv zshrc; do
   ln -sf "$origin/$f" ".$f"
 done
+
+cd $origin/config
+for f in *; do
+  ln -sf $origin/config/$f $HOME/.config/$f
+done
